589578739cdffb9c735056ea205f3456b1701586
Alef Castro <alef.castro2007@hotmail.com>
2025-08-09 00:13:07 -0300

    Update ex009.py
    Alterações pequenas.

--- exercicio/ex009.py
+++ exercicio/ex009.py
@@ -1,6 +1,8 @@
-t = int(input('Digite um numero pra ver sua Tabuada:'))
+print ('-TABUADA-')
+t = int(input('Digite um número para ver a Tabuado:'))
 
 print('_' *18)
+
 print('{} x {:2} = {}'.format(t, 1,t*1))
 print('{} x {:2} = {}'.format(t, 2,t*2))
 print('{} x {:2} = {}'.format(t, 3,t*3))
@@ -11,5 +13,6 @@ print('{} x {:2} = {}'.format(t, 7,t*7))
 print('{} x {:2} = {}'.format(t, 8,t*8))
 print('{} x {:2} = {}'.format(t, 9,t*9))
 print('{} x {:2} = {}'.format(t, 10, t*10))
+
 print('_' *18)
 
